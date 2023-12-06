ad2703746eb1b980a8e0d8c1861b06c9b8c84ec2
jessicacosta07 <jessikacostajf@gmail.com>
2023-12-06 17:45:39 -0300

    criação de componente

--- blog/src/app/app.component.ts
+++ blog/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { SmallCardComponent } from "./components/small-card/small-card.component";
+import { BigCardComponent } from "./components/big-card/big-card.component";
+import { MenuTitleComponent } from "./components/menu-title/menu-title.component";
+import { HomeComponent } from "./pages/home/home.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, HomeComponent]
 })
 export class AppComponent {
   title = 'blog';
